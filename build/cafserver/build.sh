5e54dfdeff4a3081c1f2cda6bdc336419b3aa74b
jip <jip@inspur.com>
2018-09-17 19:34:33 +0800

    caf done

--- build/cafserver/build.sh
+++ build/cafserver/build.sh
@@ -1,10 +1,10 @@
 # gsp/cloud_base_ws:0.0.1
 # gsp/cloud_caf_ws:0.0.1
 
-sudo docker pull gibbonnet/gsp2
+# sudo docker pull gibbonnet/gsp2
 
-sudo docker tag gibbonnet/gsp2 localhost:5000/gsp/cloud_base_ws:0.0.1 \
-&& sudo docker push localhost:5000/gsp/cloud_base_ws:0.0.1
+# sudo docker tag gibbonnet/gsp2 localhost:5000/gsp/cloud_base_ws:0.0.1 \
+# && sudo docker push localhost:5000/gsp/cloud_base_ws:0.0.1
 
 sudo docker build -t localhost:5000/gsp/cloud_caf_ws:0.0.1 . \
 && sudo docker push localhost:5000/gsp/cloud_caf_ws:0.0.1
--- build/cafserver/entrypoint.sh
+++
@@ -1,2 +0,0 @@
-WORKDIR /home/gsp/gsp_cloud_server/runtime/libs
-dotnet Inspur.Ecp.Caf.Communication.Service.dll
